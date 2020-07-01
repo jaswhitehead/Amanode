6900c20eedc61842e1e128056a1a92ec8e737170
Jason Whitehead <jaswhitehead@gmail.com>
2020-07-01 18:03:57 -0500

    Uploaded npm inquirer and mysql packages

--- bamazon_DB.sql
+++ bamazon_DB.sql
@@ -2,11 +2,11 @@ CREATE DATABASE bamazon_DB;
 
 USE bamazon_DB;
 
-CREATE TABLE auctions(
+CREATE TABLE products(
   id INT NOT NULL AUTO_INCREMENT,
-  item_name VARCHAR(100) NOT NULL,
-  category VARCHAR(45) NOT NULL,
-  starting_bid INT default 0,
-  highest_bid INT default 0,
+  product_name VARCHAR(200) NOT NULL,
+  department_name VARCHAR(100) NOT NULL,
+  price INT default 0,
+  stock_quantity INT default 0,
   PRIMARY KEY (id)
 );
